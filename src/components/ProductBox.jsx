bc60ada9462f6b390b8f0a135f6655d8b8e583fd
dev-atulyadav <atulyadav031103@gmail.com>
2024-03-01 13:15:04 +0530

    user home UI done

--- src/components/ProductBox.jsx
+++ src/components/ProductBox.jsx
@@ -3,21 +3,21 @@ import React from "react";
 export default function ProductBox() {
   return (
     <main className="flex flex-col justify-center items-center gap-2 p-2">
-      <div className="flex justify-between items-center p-2 rounded-xl shadow-inset-xl w-full bg-white sm:flex-col">
+      <div className="flex justify-between items-center p-4 sm:p-2 rounded-xl shadow-inset-xl bg-white sm:flex-col sm:h-56">
         <img
           src="https://onemg.gumlet.io/l_watermark_346,w_690,h_700/a_ignore,w_690,h_700,c_pad,q_auto,f_auto/cropped/n5yq2ne4k31ae9najbli.png"
-          className="h-28"
+          className="h-24"
           alt="Complain"
         />
-        <div className="text-sm font-semibold">
+        <div className="text-xs sm:text-sm sm:w-52 sm:text-center flex flex-col gap-3">
           <h3>
             Nutrigro By Complan Protein | 2 to 6 Years | Flavour Badam Kheer
           </h3>
-          <span className="flex justify-center items-center gap-2 uppercase text-white">
-            <button className="px-2 p-2 w-2/4 border-[1px]  border-blue-500 hover:bg-blue-500 hover:text-white text-blue-500 rounded-lg">
+          <span className="flex justify-center items-center gap-2 text-white">
+            <button className="px-2 py-2 w-full sm:w-24 border-[1px]  border-blue-500 hover:bg-blue-500 hover:text-white text-blue-500 rounded-lg ">
               Add to cart
             </button>
-            <button className="px-2 p-2 w-2/4 border-[1px] border-green-500 hover:bg-green-500 text-green-500 hover:text-white rounded-lg">
+            <button className="px-3 py-2 w-full sm:w-24 border-[1px] border-green-500 hover:bg-green-500 text-green-500 hover:text-white rounded-lg ">
               Buy now
             </button>
           </span>
--- src/components/UserHome.jsx
+++ src/components/UserHome.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import ProductBox from './ProductBox'
+import React from "react";
+import ProductBox from "./ProductBox";
 
 export default function UserHome() {
   return (
-    <section className='h-screen w-full bg-red-00 p-2 flex justify-center items-center'>
-      <article className='p-3 h-full w-3/4 rounded-xl shadow-inset-xl bg-gray-100 overflow-scroll flex justify-center items-start'>
-        <main className='sm:grid grid-cols-4 w-full'>
-        <ProductBox/>
-        <ProductBox/>
-        <ProductBox/>
-        <ProductBox/>
-        <ProductBox/>
-        <ProductBox/>
-        <ProductBox/>
-        <ProductBox/>
-        <ProductBox/>
-        <ProductBox/>
-        <ProductBox/>
-        <ProductBox/>
-        <ProductBox/>
-        <ProductBox/>
-        <ProductBox/>
-        <ProductBox/>
-        <ProductBox/>
-        <ProductBox/>
+    <section className=" w-full bg-red-00 p-2 flex justify-center items-center">
+      <article className="sm:p-3 h-full w-full rounded-xl shadow-inset-xl bg-gray-100 overflow-scroll flex justify-center items-start">
+        <main className="sm:flex flex-wrap justify-center items-center h-full w-full">
+          <ProductBox />
+          <ProductBox />
+          <ProductBox />
+          <ProductBox />
+          <ProductBox />
+          <ProductBox />
+          <ProductBox />
+          <ProductBox />
+          <ProductBox />
+          <ProductBox />
+          <ProductBox />
+          <ProductBox />
+          <ProductBox />
+          <ProductBox />
+          <ProductBox />
+          <ProductBox />
+          <ProductBox />
+          <ProductBox />
         </main>
       </article>
     </section>
-  )
+  );
 }
